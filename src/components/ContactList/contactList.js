0d6acc26d7a74611426429bee7a79b79efe3a840
Viktoriia Fenenko <v.l.fenenko@gmail.com>
2021-07-24 16:13:43 +0300

    Выполнение

---
+++ src/components/ContactList/contactList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './contactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import contactsActions from '../../redx/actions';
+import { getVisibleContacts } from '../../redx/selectors';
+
+export default function ContactList() {
+  const contacts = useSelector(getVisibleContacts);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = id => dispatch(contactsActions.deleteContact(id));
+
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li className={s.listItem} key={id}>
+          <span className={s.listItemText}>{name}:</span>
+          <span className={s.listItemText}>{number}</span>
+          <button
+            className={s.button}
+            type="button"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  number: PropTypes.string,
+  onDeleteContact: PropTypes.func,
+};
---
+++ src/components/Filter/filter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getFilter } from '../../redx/selectors';
+import contactsActions from '../../redx/actions';
+
+import PropTypes from 'prop-types';
+import s from './filter.module.css';
+
+const Filter = () => {
+  const value = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  return (
+    <label className={s.filterLabel}>
+      Find contacts by name
+      <input
+        className={s.filterInput}
+        type="text"
+        value={value}
+        onChange={e => dispatch(contactsActions.changeFilter(e.target.value))}
+      />
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default Filter;
